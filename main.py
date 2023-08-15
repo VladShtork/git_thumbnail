11124d6cfd5d5c1f49dfd505db4c3a7feaa35656
VladShtork <142219167+VladShtork@users.noreply.github.com>
2023-08-15 15:45:35 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from PIL import Image
 
 def main():
     im = Image.open("16. Город.jpg")
-    im.show()
+    print(im.format, im.size, im.mode)
+
+    size = (128, 128)
+    outfile = "16. Город_thumbnail.jpg"
+
+    im.thumbnail(size)
+    im.save(outfile, "PNG")
+
+    newfile = Image.open(outfile)
+    print(newfile.format, newfile.size, newfile.mode)
 
 
 if __name__ == '__main__':
-    main()
+    main()main.py
